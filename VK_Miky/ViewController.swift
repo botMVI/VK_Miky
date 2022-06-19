61b728ee59bd64836dfa1e8a085b923cd838d2da
Mike <ilmivy1024@gmail.com>
2022-06-19 18:23:23 +0600

    Initial Commit

---
+++ VK_Miky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK_Miky
+//
+//  Created by Michael Iliouchkin on 19.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
